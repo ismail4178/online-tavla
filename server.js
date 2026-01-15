f710dc4c6885f79534b86cadab500b3c4fba5546
ismail4178 <ismailbolum@gmail.com>
2026-01-15 07:36:36 +0300

    server.js

--- server.js
+++ server.js
@@ -8,28 +8,35 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-let rooms = [];
+let rooms = {};
 
 io.on("connection", socket => {
 
-  socket.emit("rooms", rooms);
-
   socket.on("createRoom", nick => {
-    const roomId = Math.random().toString(36).substr(2, 6);
-    rooms.push(roomId);
+    const roomId = Math.random().toString(36).substr(2,6);
+    rooms[roomId] = [socket.id];
     socket.join(roomId);
-
     socket.emit("roomCreated", roomId);
-    io.emit("rooms", rooms);
   });
 
   socket.on("joinRoom", roomId => {
+    if (!rooms[roomId] || rooms[roomId].length >= 2) {
+      socket.emit("roomFull");
+      return;
+    }
+    rooms[roomId].push(socket.id);
     socket.join(roomId);
+    io.to(roomId).emit("startGame");
   });
 
   socket.on("disconnect", () => {
-    // basit tutuyoruz
+    for (let r in rooms) {
+      rooms[r] = rooms[r].filter(id => id !== socket.id);
+      if (rooms[r].length === 0) delete rooms[r];
+      else io.to(r).emit("opponentLeft");
+    }
   });
+
 });
 
 server.listen(3000);
