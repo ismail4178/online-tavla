8e4788c2d93ee8e1c5716d362f800122854c2e9c
ismail4178 <ismailbolum@gmail.com>
2026-01-15 06:47:55 +0300

    game.js

--- public/game.js
+++ public/game.js
@@ -1,48 +1,19 @@
-const socket = io();
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css">
+<script src="/socket.io/socket.io.js"></script>
+</head>
 
-const board = document.getElementById("board");
+<body class="game">
+<div id="board">🎲 Tavla Tahtası</div>
 
-for (let i = 0; i < 24; i++) {
-  const p = document.createElement("div");
-  p.className = "point";
-  p.dataset.index = i;
-  board.appendChild(p);
-}
+<script>
+document.documentElement.requestFullscreen?.();
+screen.orientation?.lock("landscape");
+</script>
 
-function createStone(color) {
-  const s = document.createElement("div");
-  s.className = "stone " + color;
-  s.draggable = true;
-
-  s.ondragstart = e => {
-    e.dataTransfer.setData("color", color);
-  };
-  return s;
-}
-
-document.querySelectorAll(".point").forEach(p => {
-  p.ondragover = e => e.preventDefault();
-  p.ondrop = e => {
-    const color = e.dataTransfer.getData("color");
-    const stone = createStone(color);
-    p.appendChild(stone);
-
-    socket.emit("move", {
-      index: p.dataset.index,
-      color
-    });
-  };
-});
-
-socket.on("move", data => {
-  const p = document.querySelector(`.point[data-index="${data.index}"]`);
-  p.appendChild(createStone(data.color));
-});
-
-socket.on("startGame", () => {
-  alert("Rakip bulundu, oyun başladı!");
-});
-
-socket.on("opponentLeft", () => {
-  alert("Rakip oyundan çıktı");
-});
+<script src="game.js"></script>
+</body>
+</html>
