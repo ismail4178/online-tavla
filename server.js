0f34699d21e4796f570384e9d340fbbb164ad483
ismail4178 <ismailbolum@gmail.com>
2026-01-15 06:04:23 +0300

    server.js

--- server.js
+++ server.js
@@ -1,44 +1,37 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static("public"));
 
-const rooms = {};
+let waitingPlayer = null;
 
-io.on("connection", (socket) => {
+io.on("connection", socket => {
+  if (waitingPlayer) {
+    socket.room = waitingPlayer.room;
+    socket.join(socket.room);
 
-  socket.on("createRoom", (nick) => {
-    const code = Math.random().toString(36).substring(2,6).toUpperCase();
-    rooms[code] = { players: [{ id: socket.id, nick }] };
-    socket.join(code);
-    socket.emit("roomCreated", code);
-  });
+    io.to(socket.room).emit("startGame");
+    waitingPlayer = null;
+  } else {
+    socket.room = "room-" + socket.id;
+    socket.join(socket.room);
+    waitingPlayer = socket;
+  }
 
-  socket.on("joinRoom", ({ code, nick }) => {
-    if (!rooms[code] || rooms[code].players.length >= 2) {
-      socket.emit("errorMsg", "Oda bulunamadı veya dolu");
-      return;
-    }
-    rooms[code].players.push({ id: socket.id, nick });
-    socket.join(code);
-    io.to(code).emit("startGame", rooms[code].players);
-  });
-
-  socket.on("rollDice", (code) => {
-    const d1 = Math.ceil(Math.random() * 6);
-    const d2 = Math.ceil(Math.random() * 6);
-    io.to(code).emit("diceResult", { d1, d2 });
+  socket.on("move", data => {
+    socket.to(socket.room).emit("move", data);
   });
 
   socket.on("disconnect", () => {
-    for (const code in rooms) {
-      rooms[code].players = rooms[code].players.filter(p => p.id !== socket.id);
-      if (rooms[code].players.length === 0) delete rooms[code];
-    }
+    io.to(socket.room).emit("opponentLeft");
   });
-
 });
 
-http.listen(3000, () => console.log("Server 3000 portunda"));
+server.listen(3000, () => {
+  console.log("Tavla server çalışıyor :3000");
+});
